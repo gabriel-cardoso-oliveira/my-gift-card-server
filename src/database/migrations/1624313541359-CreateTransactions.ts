c2724464c2549d980d072ec373687ff4d4342a91
Gabriel Cardoso <gabrielfjggyn@gmail.com>
2021-06-21 20:38:52 -0300

    Migration CreateTransaction added

---
+++ src/database/migrations/1624313541359-CreateTransactions.ts
@@ -0,0 +1,45 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateTransactions1624313541359 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'transactions',
+          columns: [
+            {
+              name: 'id',
+              type: 'varchar',
+              isPrimary: true,
+              generationStrategy: 'uuid',
+            },
+            {
+              name: 'transaction_type',
+              type: 'varchar',
+              isNullable: false,
+            },
+            {
+              name: 'transaction_value',
+              type: 'numeric',
+              isNullable: false,
+            },
+            {
+              name: 'transaction_date',
+              type: 'timestamp with time zone',
+              isNullable: false,
+            },
+            {
+              name: 'store_identification',
+              type: 'varchar',
+              isNullable: false,
+            },
+          ]
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('transactions');
+    }
+
+}
